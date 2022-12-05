97ae551fd7f075bf0b59bb13c328e5d018d3cba4
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-12-05 17:36:19 +0100

    feat: numbered navigation style

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,34 @@ const App = () => {
       <header>
         <nav className='nav'>
           <ul className='nav__items' role='list'>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
+            <li className='nav__item'>
+              <span className='numbered'>1</span>
+              <div className='nav__item--info'>
+                <h4 className='title'>Step 1</h4>
+                <p className='desc'>Your Info</p>
+              </div>
+            </li>
+            <li className='nav__item'>
+              <span className='numbered'>2</span>
+              <div className='nav__item--info'>
+                <h4 className='title'>Step 2</h4>
+                <p className='desc'>Select Plan</p>
+              </div>
+            </li>
+            <li className='nav__item'>
+              <span className='numbered'>3</span>
+              <div className='nav__item--info'>
+                <h4 className='title'>Step 3</h4>
+                <p className='desc'>Pick add-ons</p>
+              </div>
+            </li>
+            <li className='nav__item'>
+              <span className='numbered'>4</span>
+              <div className='nav__item--info'>
+                <h4 className='title'>Step 4</h4>
+                <p className='desc'>Finishing up</p>
+              </div>
+            </li>
           </ul>
         </nav>
       </header>
@@ -39,13 +62,13 @@ const App = () => {
         Arcade $9/mo Advanced $12/mo Pro $15/mo Monthly Yearly Go Back Next Step
         {/* Step 2 end  */}
         {/* Step 3 start  */}
-        Pick add-ons Add-ons help enhance your gaming experience. Online service
-        Access to multiplayer games +$1/mo Larger storage Extra 1TB of cloud
-        save +$2/mo Customizable Profile Custom theme on your profile +$2/mo Go
-        Back Next Step
+        Add-ons help enhance your gaming experience. Online service Access to
+        multiplayer games +$1/mo Larger storage Extra 1TB of cloud save +$2/mo
+        Customizable Profile Custom theme on your profile +$2/mo Go Back Next
+        Step
         {/* Step 3 end  */}
         {/* Step 4 start  */}
-        Finishing up Double-check everything looks OK before confirming.
+        Double-check everything looks OK before confirming.
         {/* Dynamically add subscription and add-on selections here  */}
         Total (per month/year) Go Back Confirm
         {/* Step 4 end  */}
