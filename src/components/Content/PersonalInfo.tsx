ea9d769ba974c6b44ff7dc90b14f19d5f3ad2aa5
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-12-07 11:02:03 +0100

    feat: validate before moving on

--- src/components/Content/PersonalInfo.tsx
+++ src/components/Content/PersonalInfo.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import {PersonalType} from '../interface';
+import {useForm} from 'react-hook-form';
+import {ControlStepsPropTypes, IUserProfile} from '../interface';
 
-export default function PersonalInfo({register, errors}: PersonalType) {
+export default function PersonalInfo({
+  step,
+  updateStep,
+}: ControlStepsPropTypes) {
+  const {
+    register,
+    formState: {errors},
+    handleSubmit,
+  } = useForm<IUserProfile>();
+
+  const validateProfile = (payload: IUserProfile) => {
+    updateStep(step + 1);
+  };
   return (
     <section className='step'>
       <div>
@@ -10,46 +23,65 @@ export default function PersonalInfo({register, errors}: PersonalType) {
           Please provide your name, email address, and phone number.
         </p>
       </div>
-      <div>
-        <div className='label'>
-          <label htmlFor='name'>Name</label>
-          <span className='error'>{errors.name?.message}</span>
+      <form onSubmit={handleSubmit(validateProfile)} className='form'>
+        <div>
+          <div className='label'>
+            <label htmlFor='name'>Name</label>
+            <span className='error'>{errors.name?.message}</span>
+          </div>
+          <input
+            {...register('name', {required: 'Name is required'})}
+            className='name'
+            placeholder='e.g Stephen King'
+          />
         </div>
-        <input
-          {...register('name', {required: true})}
-          className='name'
-          placeholder='e.g Stephen King'
-        />
-      </div>
-      <div>
-        <div className='label'>
-          <label htmlFor='email'>Email Address</label>
-          <span className='error'>{errors.email?.message}</span>
+        <div>
+          <div className='label'>
+            <label htmlFor='email'>Email Address</label>
+            <span className='error'>{errors.email?.message}</span>
+          </div>
+          <input
+            {...register('email', {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/i,
+                message: 'Please provide a valid email',
+              },
+              required: 'Email is required',
+            })}
+            className='email'
+            placeholder='e.g stephenking@lorem.com'
+          />
         </div>
-        <input
-          {...register('email', {
-            pattern: {
-              value:
-                /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/i,
-              message: 'Please provide a valid email',
-            },
-            required: true,
-          })}
-          className='email'
-          placeholder='e.g stephenking@lorem.com'
-        />
-      </div>
-      <div>
-        <div className='label'>
-          <label htmlFor='phone'>Phone Number</label>
-          <span className='error'>{errors.phone?.message}</span>
+        <div>
+          <div className='label'>
+            <label htmlFor='phone'>Phone Number</label>
+            <span className='error'>{errors.phone?.message}</span>
+          </div>
+          <input
+            {...register('phone', {
+              min: {
+                message: 'Phone number must be atleast 9 digits long',
+                value: 9,
+              },
+              // https://stackoverflow.com/questions/3256547/regex-simple-phone-number
+              pattern: {
+                message: 'Invalid phone number',
+                value: /^[0-9()-]+$/i,
+              },
+              required: 'Phone number is required',
+            })}
+            className='phone'
+            placeholder='e.g +1 234 567 890'
+          />
         </div>
-        <input
-          {...register('phone', {required: true})}
-          className='phone'
-          placeholder='e.g +1 234 567 890'
-        />
-      </div>
+        <div className='controls'>
+          <div />
+          <button className='btn btn--next-page right' type='submit'>
+            Next Step
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
