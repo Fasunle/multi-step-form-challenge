e288189ac1c410041109e00778732d50addd4048
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-12-07 13:39:07 +0100

    feat: calculate total price

--- src/components/Content/Summary.tsx
+++ src/components/Content/Summary.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import {SummaryPropTypes} from '../interface';
+import {IStore, SummaryPropTypes} from '../interface';
 
 export default function Summary({
   summary,
   isYear,
   gotoStep,
+  store,
 }: {summary: SummaryPropTypes} & {
   isYear: boolean;
   gotoStep(step: number): void;
+  store: IStore;
 }) {
+  const calculator = (store: IStore): number => {
+    let total = 0;
+
+    if (isYear) {
+      total += summary.yearly;
+      store.subscription.addOns.forEach((item) => (total += item.yearly));
+    } else {
+      total += summary.monthly;
+      store.subscription.addOns.forEach((item) => (total += item.monthly));
+    }
+
+    return total;
+  };
+
   return (
     <section className='step'>
       <div>
@@ -26,20 +42,26 @@ export default function Summary({
               Change
             </button>
           </h5>
-          <p className='price'>{isYear ? summary.yearly : summary.monthly}</p>
+          <p className='price'>
+            {isYear ? `$${summary.yearly}/yr` : `$${summary.monthly}/mo`}
+          </p>
         </div>
         <hr className='divider' />
         {summary.addOns.map((addOn) => (
           <div className='type' key={addOn.title}>
             <h6 className='title'>{addOn.title}</h6>
-            <p className='price'>{isYear ? addOn.yearly : addOn.monthly}</p>
+            <p className='price'>
+              {isYear ? `${addOn.yearly}/yr` : `$${summary.monthly}/mo`}
+            </p>
           </div>
         ))}
       </div>
 
       <div className='total'>
         <span className='title'>Total (per Month)</span>
-        <span className='amount'>+$12/mo</span>
+        <span className='amount'>
+          {isYear ? `$${calculator(store)}` : `$${calculator(store)}`}
+        </span>
       </div>
     </section>
   );
